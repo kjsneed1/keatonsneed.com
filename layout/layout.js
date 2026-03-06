d420c36a4ab608533b5be60e8ab0f2704708eb84
Keaton Sneed <kjsneed2@gmail.com>
2026-03-05 22:08:48 -0600

    Made page links load dynamically

--- layout/layout.js
+++ layout/layout.js
@@ -36,8 +36,18 @@ const clickOffNav = function () {
     }
 };
 
+const setRoute = async function(route){
+    window.history.pushState({} , "", route)
+    const newPage = await (await fetch(route)).text()
+    let newContent = document.createElement("html")
+    newContent.innerHTML = newPage
+    document.getElementsByClassName("content")[0].innerHTML = ""
+    newContent = newContent.children[0].children[2].content
+    document.getElementsByClassName("content")[0].appendChild(newContent)
+}
+
 void (async function () {
-    var layout = document.createElement("html");
+    const layout = document.createElement("html");
     layout.innerHTML = await (await fetch("/layout/layout.html")).text();
     document.body.appendChild(layout);
 
@@ -46,4 +56,10 @@ void (async function () {
 
     let content = document.getElementById("contentTemp").content.cloneNode(true);
     document.getElementsByClassName("content")[0].appendChild(content)
+    const navButtons = document.getElementsByClassName("nav")[0].children
+    for (let a of navButtons){
+        const href = a.href
+        a.setAttribute('onclick', `void(async ()=> await setRoute('${href}'))()`)
+        a.removeAttribute("href")
+    }
 })();
