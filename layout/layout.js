1e31f28cc7fe0211b8bd3c5e0bbc14a46a599c3b
Keaton Sneed <kjsneed2@gmail.com>
2026-03-09 22:23:49 -0500

    Add inital forward and back logic to custom routing

--- layout/layout.js
+++ layout/layout.js
@@ -36,7 +36,7 @@ const clickOffNav = function () {
     }
 };
 
-function linkStyle(href){
+function linkStyle(href) {
     let styleLink = document.createElement("link");
     styleLink.rel = "stylesheet";
     styleLink.type = "text/css";
@@ -44,35 +44,46 @@ function linkStyle(href){
     document.getElementsByTagName("HEAD")[0].appendChild(styleLink);
 }
 
-const setRoute = async function(route){
-    window.history.pushState({} , "", route)
-
-    let newContent = document.createElement("html")
-    newContent.innerHTML = await (await fetch(route)).text()
-
-    const scripts = newContent.querySelectorAll("script.important")
-
-    for(let s of scripts){
-        const script = document.createElement("script")
-        if(s.src !== ""){script.src = s.src}
-        script.text =  s.text
-        document.getElementsByTagName("HEAD")[0].appendChild(script)
+const setRoute = async function (route, pop = false) {
+    if (!pop) {
+        window.history.pushState(route, "", route);
     }
 
-    document.getElementsByClassName("content")[0].innerHTML = ""
+    let newContent = document.createElement("html");
+    newContent.innerHTML = await (await fetch(route)).text();
 
-    const contents = newContent.querySelectorAll(":not(script).important")
+    const scripts = newContent.querySelectorAll("script.important");
 
-    for(let c of contents){
-        document.getElementsByClassName("content")[0].appendChild(c)
+    for (let s of scripts) {
+        const script = document.createElement("script");
+        if (s.src !== "") {
+            script.src = s.src;
+        }
+        script.text = s.text;
+        document.getElementsByTagName("HEAD")[0].appendChild(script);
+    }
+
+    document.getElementsByClassName("content")[0].innerHTML = "";
+
+    const contents = newContent.querySelectorAll(":not(script).important");
+
+    for (let c of contents) {
+        document.getElementsByClassName("content")[0].appendChild(c);
     }
 
     document.getElementById("pageTitle").text = pageTitle;
     document.getElementById("headerTitle").textContent = pageTitle;
-}
+    clickOffNav();
+};
 
+window.history.pushState(location.pathname, "");
 
-linkStyle("/layout/layout.css")
+onpopstate = (event) => {
+    console.log("here");
+    setRoute(event.state, true);
+};
+
+linkStyle("/layout/layout.css");
 
 void (async function () {
     const layout = document.createElement("html");
@@ -80,12 +91,15 @@ void (async function () {
     document.body.appendChild(layout);
 
     let content = document.querySelector("div.important");
-    document.getElementsByClassName("content")[0].appendChild(content)
-    const navButtons = document.getElementsByClassName("nav")[0].children
-    for (let a of navButtons){
-        const href = a.href
-        a.setAttribute('onclick', `void(async ()=> await setRoute('${href}'))()`)
-        a.removeAttribute("href")
+    document.getElementsByClassName("content")[0].appendChild(content);
+    const navButtons = document.getElementsByClassName("nav")[0].children;
+    for (let a of navButtons) {
+        const href = a.href;
+        a.setAttribute(
+            "onclick",
+            `void(async ()=> await setRoute('${href}'))()`,
+        );
+        a.removeAttribute("href");
     }
 
     document.getElementById("pageTitle").text = pageTitle;
